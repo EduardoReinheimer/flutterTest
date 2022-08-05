488e466dbf1bdfa670c52885079ea784414c945d
Eduardo Salvador Reinheimer <du428@hotmail.com>
2022-08-05 10:06:05 -0300

    Adding a Infinite Scroll

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,37 @@ void main() {
   runApp(const MyApp());
 }
 
+class RandonWords extends StatefulWidget {
+  const RandonWords({super.key});
+
+  @override
+  State<RandonWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandonWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFront = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+          final index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFront,
+            ),
+          );
+        });
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,13 +43,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandonWords(),
         ),
       ),
     );
